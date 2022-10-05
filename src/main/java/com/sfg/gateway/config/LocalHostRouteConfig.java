6b2547161c890b6b526e6ab6687cb3295afc026a
vitor <vitorgilcsgmail.com>
2022-10-05 01:29:08 +0100

    Add Route for Inventory Service

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -9,14 +9,17 @@ import org.springframework.context.annotation.Configuration;
 public class LocalHostRouteConfig {
 
     @Bean
-    public RouteLocator localHostRoute(RouteLocatorBuilder builder){
+    public RouteLocator localHostRoute(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*" )
-                     .uri("http://localhost:8080")
-                     .id("beer-service"))
-                .route( r -> r.path( "/api/v1/customers/**")
-                    .uri("http://localhost:8081")
-                    .id("order-service"))
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("http://localhost:8080")
+                        .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082")
+                        .id("inventory-service"))
                 .build();
     }
 }
